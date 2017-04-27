1f243032c68855a0d593edcace3be22a9cd2b741
Aidan <miytepithon@gmail.com>
2017-04-26 22:39:23 -0400

    Finalized Minigame

--- game/GaiaUndertaking/Assets/playerController.cs
+++ game/GaiaUndertaking/Assets/playerController.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class playerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
+        
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -22,4 +30,23 @@ public class playerController : MonoBehaviour {
         rb.AddForce(movement * speed);
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 14)
+        {
+            winText.text = "You Win!";
+        }
+    }
+    //Destroy(other.GameObject);
 }
